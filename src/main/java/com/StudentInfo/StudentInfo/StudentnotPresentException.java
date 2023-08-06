b4cc4834eabd89d04d6b3e0d875aa23a50d57b08
Manoj <gmk16020ee022@gmail.com>
2023-08-07 03:44:17 +0530

    added one to many

--- src/main/java/com/StudentInfo/StudentInfo/StudentnotPresentException.java
+++
@@ -1,9 +0,0 @@
-package com.StudentInfo.StudentInfo;
-
-public class StudentnotPresentException extends Exception {
-
-    public StudentnotPresentException(String msg){
-        super(msg) ;
-    }
-
-}
